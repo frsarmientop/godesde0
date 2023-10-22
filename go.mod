ccd8a9b73c45050be67cbcf17d9384f8918f502a
Freddy <frsarmientop@gmail.com>
2023-10-22 13:45:27 -0300

    PrimerCommit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module helloworld/github.com/frsarmientop/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"helloworld/github.com/frsarmientop/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
