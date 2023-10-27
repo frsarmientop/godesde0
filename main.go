dec2b075eca0a5327d33524a54d9a1ca7e8c6673
Freddy <frsarmientop@gmail.com>
2023-10-26 23:43:07 -0300

    segundoCommit

--- main.go
+++ main.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
-	"helloworld/github.com/frsarmientop/godesde0/variables"
+	"helloworld/github.com/frsarmientop/godesde0/ejercicios"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(1500)
+	/*estado, texto := variables.ConviertoaTexto(1500)
 	fmt.Println(estado)
+	fmt.Println(texto)*/
+
+	/*if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("Esto no es windows, es ", os)
+
+	} else {
+		fmt.Println("Esto no es windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es linux")
+	case "darwin":
+		fmt.Println("Esto es darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
+	*/
+	numero, texto := ejercicios.ConvNumerico("fff")
+	fmt.Println(numero)
 	fmt.Println(texto)
 }
